db3faaa64ce84f9dbcb74f76523904f35d2d2eee
etosolo322 <etosolo322@yandex.ru>
2022-04-19 23:37:59 +0300

    master

---
+++ src/assets/database/base.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB4pqlnBwJHByltGk9PtZM6AnNShqwwY2M",
+    authDomain: "bzsp-5c9ae.firebaseapp.com",
+    databaseURL: "https://bzsp-5c9ae-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "bzsp-5c9ae",
+    storageBucket: "bzsp-5c9ae.appspot.com",
+    messagingSenderId: "373726371712",
+    appId: "1:373726371712:web:0a02909d84d1654b486b65",
+    measurementId: "G-YYKXC1ZVNL"
+  };

d1f9d6519e62e9cd75544e1c4021be4515fdedf1
etosolo322 <etosolo322@yandex.ru>
2022-05-02 19:49:54 +0300

    about

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Header} from "./container/header"
+import {NotFound} from "./container/404"
 import Homepage from "./page/homepage/homepage"
 import Hobby from "./page/hobby/hobby"
 import About from "./page/about/aboutMe"
@@ -12,6 +13,7 @@ function App() {
       <Route path='/' element={<Header/>}>
           <Route index element={<Homepage/>}/>
           <Route path='hobby' element={<Hobby/>}/>
+          <Route path='*' element={<NotFound/>}/>
       </Route>
         <Route path='about' element={<About />}/>
       </Routes>

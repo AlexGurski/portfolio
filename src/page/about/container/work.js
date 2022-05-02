bb5ccbbe71917e28dd619830eed82ce531144828
etosolo322 <etosolo322@yandex.ru>
2022-05-02 12:19:31 +0300

    work

--- src/page/about/container/work.js
+++ src/page/about/container/work.js
@@ -6,12 +6,29 @@ export const MyWork = () => {
     return (
     <div className="about_about" id='work'>
         <div className="about_container" data-aos="fade-up" data-aos-duration="2000">
-            <p>My first serious project is a thesis.</p>
-            <p>This project was developed for the Baranovichi cafe "Park Avenue". It was a FULL STACK development. Starting from the design and Frontend and ending with the server part (NodeJS) with a database (MongoDB).</p>
+            <p>My first project, after which I fell in love with web development, was a course work "website for a pizzeria".</p>
+            <p>I found the design I liked, little modified it and made it up in its likeness.</p>
+            <p>Having opened it now, I understand that it is not so beautiful, but then, seeing the result of my own work, I wanted to engage in web development.</p>
         </div>
         <div className="about_container" data-aos="fade-up" data-aos-duration="2000">
-            <p>Currently, it is impossible to imagine your life without the Internet. We use it all the time: at home, on the way to and from work, at work. And what are we looking at on the Internet? to websites. All self-respecting companies have them and their number is only increasing. And companies that already have websites will improve, redo and supplement them over time.</p>
-            <p>In the army, there is no room for creativity and a sense of admiration for the work done, which gives web development.</p>
+            <p>My first serious project is a thesis.</p>
+            <p>This project was developed for the Baranovichi cafe "Park Avenue". It was a FULL STACK development. Starting from the design and Frontend and ending with the server part (NodeJS) with a database (MongoDB).</p>
+            <p>Upon completion, the site was hosted and was available on the Internet for a year. But when the domain expired, the cafe director decided not to renew it because of the high cost.
+            At the moment when the site was available on the Internet, I was proud that absolutely anyone could see what I had done.</p>
+            <p>And I wanted to continue doing web development, but the main job did not allow me to pay much attention to programming, I was also afraid to get out of my comfort zone and left everything as it is.</p>
+        </div>
+        <div className="about_container" data-aos="fade-up" data-aos-duration="2000">
+            <p>My very first project, after which I fell in love with web development, was a course work "website for a pizzeria".</p>
+            <p>I found the design I liked, little modified it and made it up in its likeness.</p>
+            <p>Having opened it now, I understand that it is not so beautiful, but then, seeing the result of my own work, I wanted to engage in web development.</p>
+        </div>
+        <div className="about_container" data-aos="fade-up" data-aos-duration="2000">
+            <p>The year 2022 has come. I was already tired of the army, more and more restrictions were being introduced there, and I decided to return to what I like - web development. In addition, the February events pushed me to this even more.</p>
+            <p>I decided to catch up, remember the old and learn something new. So I decided to try to write something on React Native in order to put it on Google Play in the future. So the idea came up to make a mobile game like "Who wants to become a millionaire", only on a football theme, as it is close to me. At the moment, the game is a little more than 60% complete, all because of a new thesis...</p>
+        </div>
+        <div className="about_container" data-aos="fade-up" data-aos-duration="2000">
+            <p>This year, a friend of mine is finishing his studies at the university and invited me to do a thesis "website for the Baranovichi plant". And I agreed.</p>
+            <p>To begin with, I opened their current website and got my 2007 back. Based on several existing designs of industrial sites, I developed my own and got to work. The main library for development was React, as it is slightly familiar to me from React Native, and it is quite popular, which is not unimportant when looking for a job. I also used css, which I was afraid of when developing my own work, but it turned out that this preprocessor significantly facilitates the life of the Frontend developer.</p>
         </div>
     </div>
     )

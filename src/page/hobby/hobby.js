d8a3b1f64c5c00b5f32b2363c59cf8c8f1fff3da
etosolo322 <etosolo322@yandex.ru>
2022-05-03 13:21:14 +0300

    hobby

--- src/page/hobby/hobby.js
+++ src/page/hobby/hobby.js
@@ -2,7 +2,7 @@ import React,{useEffect} from "react";
 import "./style/hobby.css"
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
+import 'flag-icons'
 const Hobby = () => {
     useEffect(()=>{
       AOS.init();
@@ -18,173 +18,80 @@ const Hobby = () => {
             </div>
             </div>
             <div className="section">
+            <div className="p fixed">
+                <h1>TRAVELING</h1>
+                <h3>Only two things we will regret on deathbed – that we are a little loved and little traveled.</h3>
+            </div>
+            </div>
+            <div className="section">
+            <div className="p fixed hobby_frame" style={{display:"flex", justifyContent:'center'}}>
+                <div className="travel_container">            
+                    <div><img src={require('../../assets/image/travel/barca.jpg')}/><span class="fi fi-es"></span></div>
+                    <div><img src={require('../../assets/image/travel/lviv.jpg')}/><span class="fi fi-ua"></span></div>
+                    <div><img src={require('../../assets/image/travel/innsbruck.jpg')}/><span class="fi fi-at"></span></div>
+                    <div><img src={require('../../assets/image/travel/it.jpg')}/><span class="fi fi-it"></span></div>   
+                    <div><img src={require('../../assets/image/travel/fr.jpg')}/><span class="fi fi-fr"></span></div>  
+                    <div><img src={require('../../assets/image/travel/girona.jpg')}/><span class="fi fi-es"></span></div>
+                    <div><img src={require('../../assets/image/travel/pl.jpg')}/><span class="fi fi-pl"></span></div>
+                    <div><img src={require('../../assets/image/travel/ger.jpg')}/><span class="fi fi-de"></span></div>
+                    <div><img src={require('../../assets/image/travel/vilna.jpg')}/><span class="fi fi-lv"></span></div>
+                    <div><img src={require('../../assets/image/travel/cat.jpg')}/><span class="fi fi-es-ct"></span></div>                   
+                    <div><img src={require('../../assets/image/travel/cz.jpg')}/><span class="fi fi-cz"></span></div>  
+                    <div><img src={require('../../assets/image/travel/gervy.jpg')}/><span class="fi fi-by"></span></div>  
+                    <div><img src={require('../../assets/image/travel/uk.jpg')}/><span class="fi fi-ua"></span></div>
+                    <div><img src={require('../../assets/image/travel/mald.jpg')}/><span class="fi fi-by"></span></div>
+                    <div><img src={require('../../assets/image/travel/thrones.jpg')}/><span class="fi stark"></span></div>
+                </div> 
+            </div>
+            </div>
+            <div className="section">
+            <div className="trevel fixed p">
+            <h1>GASTRO TRIP</h1>
+            <h3>What's the point of traveling if you don't taste their food</h3>
+            </div>
+            </div>
+            <div className="section">
+            <div className="p fixed hobby_frame" >
+            <div className="gastro_container">
+                <div className="box">
+                    <img src={require('../../assets/image/gastro/barca.jpg')}/>
+                    <span>Barcelona</span>
+                </div>               
+                <div className="box">
+                    <img  src={require('../../assets/image/gastro/vilna.jpg')} />
+                    <span>Vilnius</span>
+                </div>
+                <div className="box">
+                    <img src={require('../../assets/image/gastro/lviv1.jpg')}/>
+                    <span>Lviv</span>
+                </div>
+                <div className="box">
+                    <img src={require('../../assets/image/gastro/venezia.jpg')}/> 
+                    <span>Venezia</span>
+                </div>
+                <div className="box">
+                    <img src={require('../../assets/image/gastro/mir.jpg')}/> 
+                    <span>Mir</span>
+                </div>
+            </div>
+            </div>
+            </div>
+            <div className="section">
             <div className="fixed">
                 <h1>SNOWBOADRING</h1>
                 <h3>The board makes you fall in love with winter and look forward to its arrival.</h3>
             </div>
             </div>
             <div className="section">
-            <div className="p fixed">
-                <ul className="img-list">
-                        <li><span className="img-placeholder"></span></li>
-                        <li><img src={require('../../assets/image/snowboard6.jpg')}/></li>
-                        <li><img src={require('../../assets/image/snowboard1.jpg')}/></li>
-                        <li><img src={require('../../assets/image/snowboard3.jpg')}/></li>
-                        <li><span className="img-placeholder"></span></li>
-                        <li><img src={require('../../assets/image/snowboard5.jpg')}/></li>
-                        <li><span className="img-placeholder"></span></li>
-                        <li><img src={require('../../assets/image/snowboard2.jpg')}/></li>
-                        <li><img src={require('../../assets/image/snowboard4.jpg')}/></li>
+            <div className="snow">
+                <ul className="img-list">                        
+                        <li><img src={require('../../assets/image/snow/snowboard6.jpg')}/></li>
+                        <li><img src={require('../../assets/image/snow/snowboard1.jpg')}/></li>
+                        <li><img src={require('../../assets/image/snow/snowboard3.jpg')}/></li>                       
+                        <li><img src={require('../../assets/image/snow/snowboard5.jpg')}/></li>                        
+                        <li><img src={require('../../assets/image/snow/snowboard2.jpg')}/></li>
+                        <li><img src={require('../../assets/image/snow/snowboard4.jpg')}/></li>
                 </ul>
-            </div>
-            </div>
-            <div className="section">
-            <div className="trevel fixed p">
-            <h1>TRAVELING</h1>
-            <h3>Only two things we will regret on deathbed – that we are a little loved and little traveled.</h3>
-            </div>
-            </div>
-            <div className="section">
-            <div className="p fixed">
-            <div className="container">
-                <div className="box">
-                    <img src={require('../../assets/image/Barcelona.jpg')}/>
-                    <span>Barcelona</span>
-                </div>
-                <div className="box">
-                    <img src={require('../../assets/image/Paris.jpg')}/>
-                    <span>Paris</span>
-                </div>
-                <div className="box">
-                    <img src={require('../../assets/image/Girona.jpg')}/>
-                    <span>Girona</span>
-                </div>
-                <div className="box">
-                    <img src={require('../../assets/image/Venezia.jpg')}/>
-                    <span>Venezia</span>
-                </div>
-                <div className="box">
-                    <img src={require('../../assets/image/Innsbruck.jpg')}/>
-                    <span>Innsbruck</span>
-                </div>
-            </div>
-            </div>
-            </div>
-            <div className="section">
-            <div className="p fixed">
-            <h1>GASTRO TRIP</h1>
-            <h3>What's the point of traveling if you don't taste their food</h3>
-            </div>
-            </div>
-            <div className="section">
-            <div className="p fixed" style={{display:"flex", justifyContent:'center'}}>
-            <div className="gastro_trip">
-            <figure class="snip0016">
-                <img src={require('../../assets/image/gastro/barca.jpg')} style={{width:'100%', height:'100%'}}/>    
-                <figcaption>
-                    <h2>Barcelona</h2>
-                    <p>Spain</p>                    
-                </figcaption>			
-            </figure>   
-            <figure class="snip0016">
-             <img src={require('../../assets/image/gastro/girona.jpg')} style={{width:'100%', height:'100%'}}/>        
-                <figcaption>
-                    <h2>Girona</h2>
-                    <p>Spain</p>                    
-                </figcaption>			
-            </figure>           
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/lloret.jpg')} style={{width:'100%', height:'100%'}}/>        
-                <figcaption>
-                    <h2>Lloret de Mar</h2>
-                    <p>Spain</p>                    
-                </figcaption>			
-            </figure>
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/venezia.jpg')} style={{width:'100%', height:'100%'}}/>       
-                <figcaption>
-                    <h2>Venezia</h2>
-                    <p>Italy</p>                    
-                </figcaption>			
-            </figure>
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/vilna.jpg')} style={{width:'100%', height:'100%'}}/>       
-                <figcaption>
-                    <h2>Vilnius</h2>
-                    <p>Litvania</p>                    
-                </figcaption>			
-            </figure>
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/aus.jpg')} style={{width:'100%', height:'100%'}}/>      
-                <figcaption>
-                    <h2>Innsbruck</h2>
-                    <p>Austria</p>                    
-                </figcaption>			
-            </figure>  
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/lviv.jpg')} style={{width:'100%', height:'100%'}}/>    
-                <figcaption>
-                    <h2>Lviv</h2>
-                    <p>Ukraine</p>                    
-                </figcaption>			
-            </figure>  
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/lviv1.jpg')} style={{width:'100%', height:'100%'}}/>    
-                <figcaption>
-                    <h2>Lviv</h2>
-                    <p>Ukraine</p>                    
-                </figcaption>			
-            </figure> 
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/mir.jpg')} style={{width:'100%', height:'100%'}}/>    
-                <figcaption>
-                    <h2>Mir</h2>
-                    <p>Belarus</p>                    
-                </figcaption>			
-            </figure> 
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/lida.jpg')} style={{width:'100%', height:'100%'}}/>   
-                <figcaption>
-                    <h2>Lida</h2>
-                    <p>Belarus</p>                    
-                </figcaption>			
-            </figure> 
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/berlin.jpg')} style={{width:'100%', height:'100%'}}/> 
-                <figcaption>
-                    <h2>Berlin</h2>
-                    <p>Germany</p>                    
-                </figcaption>			
-            </figure> 
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/home.jpg')} style={{width:'100%', height:'100%'}}/>  
-                <figcaption>
-                    <h2>Home</h2>
-                    <p>Sweet home</p>                    
-                </figcaption>			
-            </figure>
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/bar.jpg')} style={{width:'100%', height:'100%'}}/>  
-                <figcaption>
-                    <h2>Baranovichy</h2>
-                    <p>Belarus</p>                    
-                </figcaption>			
-            </figure>
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/minsk.jpg')} style={{width:'100%', height:'100%'}}/>  
-                <figcaption>
-                    <h2>Minsk</h2>
-                    <p>Belarus</p>                    
-                </figcaption>			
-            </figure>
-            <figure class="snip0016">
-            <img src={require('../../assets/image/gastro/grodno.jpg')} style={{width:'100%', height:'100%'}}/>
-                <figcaption>
-                    <h2>Grodno</h2>
-                    <p>Belarus</p>                    
-                </figcaption>			
-            </figure>  
-            </div> 
             </div>
             </div>
             <div className="section">

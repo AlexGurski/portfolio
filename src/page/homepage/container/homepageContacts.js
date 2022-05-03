9b811c0e7f68735fb5294ad1aa5aa404114949b1
etosolo322 <etosolo322@yandex.ru>
2022-05-03 09:07:00 +0300

    about

--- src/page/homepage/container/homepageContacts.js
+++ src/page/homepage/container/homepageContacts.js
@@ -9,13 +9,14 @@ const HomepageContacts = () => {
     return (
         <div className="homepage_contacts">
             <div className="homepage__contacts">
-                <div><SiVk/></div>
-                <div><SiTelegram/></div>
-                <div><SiLinkedin/></div>
-                <div><SiInstagram/></div>
-                <div><SiMaildotru/></div>
+                <div><a href='https://vk.com/id54471278'target='_blank'><SiVk/></a></div>
+                <div><a href='https://t.me/+375298209524'target='_blank'><SiTelegram/></a></div>
+                <div><a href='http://www.linkedin.com/in/alexandr-gurski-b2ab21195/'target='_blank'> <SiLinkedin/></a></div>
+                <div><a href='https://www.instagram.com/alex7gurski' target='_blank'><SiInstagram/></a></div>
+                <div><a href="https://mailto:etosolo322@yandex.ru" target='_blank'><SiMaildotru/></a></div>
             </div>
         </div>
+        
     ) 
 }
 export default HomepageContacts

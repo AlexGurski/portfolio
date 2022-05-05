ce34455bdd5b85b393b2d53008dc2c4e8dd59c14
etosolo322 <etosolo322@yandex.ru>
2022-05-05 22:33:47 +0300

    master

--- src/page/about/container/contacts.js
+++ src/page/about/container/contacts.js
@@ -3,21 +3,21 @@ import "../style/about.css"
 import {SiVk, SiTelegram, SiLinkedin, SiInstagram, SiMaildotru} from 'react-icons/si'
 
 export const Contacts = () => {
-    const [mail,setMail] = useState({opacity:0})
-    const [vk,setVk] = useState({opacity:0})
-    const [tele,setTele] = useState({opacity:0})
-    const [linked,setLinked] = useState({opacity:0})
-    const [inst,setInst] = useState({opacity:0})
+    const [mail,setMail] = useState({opacity:0,pointerEvents:'none'})
+    const [vk,setVk] = useState({opacity:0,pointerEvents:'none'})
+    const [tele,setTele] = useState({opacity:0,pointerEvents:'none'})
+    const [linked,setLinked] = useState({opacity:0,pointerEvents:'none'})
+    const [inst,setInst] = useState({opacity:0,pointerEvents:'none'})
 
     return (
     <div className="about_about" id='contacts'>
          <div className="about_container about_contacts" >
          <div className="about__contacts">
-                <div><SiVk onClick={()=>setVk({opacity:1})}/><a href='https://vk.com/id54471278'target='_blank' style={vk}>id54471278</a></div>
-                <div><SiTelegram onClick={()=>setTele({opacity:1})}/><a href='https://t.me/+375298209524'target='_blank' style={tele}>@alexgurski</a></div>
-                <div><SiLinkedin onClick={()=>setLinked({opacity:1})}/><a href='http://www.linkedin.com/in/alexandr-gurski-b2ab21195/'target='_blank' style={linked}>alexandr-gurski-b2ab21195</a></div>
-                <div><SiInstagram onClick={()=>setInst({opacity:1})}/><a  href='https://www.instagram.com/alex7gurski' target='_blank' style={inst}>@alex7gurski</a></div>
-                <div><SiMaildotru onClick={()=>setMail({opacity:1})}/><a  href="https://mailto:etosolo322@yandex.ru" target='_blank' style={mail}>etosolo322@yandex.ru</a></div>
+                <div><SiVk onClick={()=>setVk({opacity:1, pointerEvents:'auto'})}/><a href='https://vk.com/id54471278'target='_blank' style={vk}>id54471278</a></div>
+                <div><SiTelegram onClick={()=>setTele({opacity:1, pointerEvents:'auto'})}/><a href='https://t.me/+375298209524'target='_blank' style={tele}>@alexgurski</a></div>
+                <div><SiLinkedin onClick={()=>setLinked({opacity:1, pointerEvents:'auto'})}/><a href='http://www.linkedin.com/in/alexandr-gurski-b2ab21195/'target='_blank' style={linked}>alexandr-gurski-b2ab21195</a></div>
+                <div><SiInstagram onClick={()=>setInst({opacity:1, pointerEvents:'auto'})}/><a  href='https://www.instagram.com/alex7gurski' target='_blank' style={inst}>@alex7gurski</a></div>
+                <div><SiMaildotru onClick={()=>setMail({opacity:1, pointerEvents:'auto'})}/><a  href="https://mailto:etosolo322@yandex.ru" target='_blank' style={mail}>etosolo322@yandex.ru</a></div>
             </div>
         </div>
     </div>
